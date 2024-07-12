0ef373585cdd07a76cfb22e663ccb0d138a37772
Justin Furuness <jfuruness@gmail.com>
2024-07-11 21:38:32 -0400

    added ROV config

--- phd_defense/tests/engine_tests/engine_test_configs/__init__.py
+++ phd_defense/tests/engine_tests/engine_test_configs/__init__.py
@@ -1,7 +1,9 @@
 from .config_001 import config_001
+from .config_002 import config_002
+from .config_003 import config_003
 
 
-engine_test_configs = (config_001,)
+engine_test_configs = (config_001, config_002, config_003)
 
 __all__ = [
     "engine_test_configs",
