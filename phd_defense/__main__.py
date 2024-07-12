728a9a26018b0dcb4d2347dc2d904d0b944b14f9
Justin Furuness <jfuruness@gmail.com>
2024-07-11 23:21:00 -0400

    phd defenses

--- phd_defense/__main__.py
+++ phd_defense/__main__.py
@@ -1,8 +1,37 @@
+from multiprocessing import cpu_count
+from pathlib import Path
 import time
+
+from bgpy.simulation_engine import ROV, ROVPPV1Lite
+from bgpy.enums import SpecialPercentAdoptions  # noqa
+from bgpy.simulation_framework import (
+    Simulation,
+    SubprefixHijack,
+    ScenarioConfig,
+)
 
 
 def main():
-    pass
+
+    sim = Simulation(
+        percent_adoptions=(
+            # SpecialPercentAdoptions.ONLY_ONE,
+            0.01,
+            0.1,
+            0.2,
+            0.5,
+            0.8,
+            0.99,
+        ),
+        scenario_configs=(
+            ScenarioConfig(ScenarioCls=SubprefixHijack, AdoptPolicyCls=ROV),
+            ScenarioConfig(ScenarioCls=SubprefixHijack, AdoptPolicyCls=ROVPPV1Lite),
+        ),
+        output_dir=Path("~/Desktop/phd_defense").expanduser(),
+        num_trials=100,
+        parse_cpus=cpu_count() - 2,
+    )
+    sim.run()
 
 
 if __name__ == "__main__":
